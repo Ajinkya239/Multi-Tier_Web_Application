5f594fd5670a1732b747f5d1a52a086328fddd8e
Ajinkya Patil <redhatajinkya@gmail.com>
2024-08-27 20:18:58 +0530

    Update keypair.tf

--- terraform/keypair.tf
+++ terraform/keypair.tf
@@ -1,4 +1,3 @@
 resource "aws_key_pair" "ssh_key" {
   key_name   = "secure-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAG7+UDshWZ+GgCtVoBTGt9dP2mg5EPMKPRQ+1yuiBdY ajinkyapatil@AJ-Macbook.local"
-}
+  public_key = "<add your public key here>"
